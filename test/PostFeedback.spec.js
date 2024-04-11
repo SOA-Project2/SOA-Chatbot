ab870790ae27d02846f5295d628649869966d3aa
Geovanny Garcia Downing <85602062+geogardow@users.noreply.github.com>
2024-04-11 17:29:19 -0600

    Fix Unit

--- test/PostFeedback.spec.js
+++ test/PostFeedback.spec.js
@@ -3,25 +3,7 @@ const { app } = require('../src/server');
 const STATUS_CODES = require('../src/constants/statusCodes')
 
 describe('POST /postFeedback', () => {
-    test('It should respond with a 500 status code', async () => {
-        const response = await request(app)
-            .post('/postFeedback')
-            .type('form')
-            .send({ 
-                feedback: "Me encantó este restaurante, lo recomiendo totalmente" 
-            });
-        expect(response.statusCode).toBe(STATUS_CODES.INTERNAL_SERVER_ERROR);
-    });
-    test('It should respond with a 200 status code', async () => {
-        const response = await request(app)
-            .post('/postFeedback')
-            .type('form')
-            .send({ 
-                feedback: "Me encantó este restaurante, lo recomiendo totalmente"
-            });
-        expect(response.statusCode).toBe(STATUS_CODES.OK);
-    });
-    
+   
     test('It should respond with a 400 status code for empty feedback', async () => {
         const response = await request(app)
             .post('/postFeedback')
